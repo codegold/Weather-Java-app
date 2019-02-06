8044d04e74c6d337d1ba13236ccc7f1d9a1a17c6
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2019-02-06 11:33:51 +0300

    Need to fix 1st day bug

--- Weather/src/main/java/Parser.java
+++ Weather/src/main/java/Parser.java
@@ -5,6 +5,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
@@ -15,24 +16,63 @@ public class Parser {
     }
 
     //from this 04.02 Понедельник погода сегодня -> get 04.02
-    private Pattern pattern = Pattern.compile("\\d{2}.\\d{2}") //Паттерн - механизм шаблон чтобы
+    private static Pattern pattern = Pattern.compile("\\d{2}.\\d{2}"); //Паттерн - механизм шаблон чтобы
+
     // искать нужную инфо в тексте. Матчер - ищет, матчит нужную инфо в тексте
     // \d{2}.\d{2}  - d значит 2 символа
-    private String getDateFromString(String stringDate) {
-
+    private static String getDateFromString(String stringDate) throws Exception {
+        Matcher matcher = pattern.matcher(stringDate);
+        if (matcher.find()) { //если матчер что-то нашел
+            return matcher.group(); //верни и сгруппируй найденное
+        }
+        throw new Exception("Can't extract date from string!");
     }
 
-    public static void main(String[] args) throws IOException {
+    private static int printPartValues(Elements values, int index) {
+        int iterationCount = 4;
+        if (index == 0) {
+            Element valueLn = values.get(3); ////Хотим из Вэльюз забрать по индексу
+            boolean isMorning = valueLn.text().contains(" Утро");
+            if (isMorning) {
+                iterationCount = 3;
+            }
+
+            for (int i = 0; i < iterationCount; i++) {
+                Element valueLine = values.get(index + i);
+                for (Element td : valueLn.select("td")) {
+                    System.out.print(td.text() + "    ");
+                }
+                System.out.println();
+            }
+            return iterationCount;
+        } else {
+            /**Дублирурующийся код - переработать чтобы не было */
+            for (int i = 0; i < iterationCount; i++) {
+                Element valueLine = values.get(index + i); //Хотим из Вэльюз забрать по индексу
+                for (Element td : valueLine.select("td")) {
+                    System.out.print(td.text() + "    ");
+                }
+                System.out.println();
+            }
+        }
+        return iterationCount;
+    }
+
+    public static void main(String[] args) throws Exception {
         Document page = getPage();
         //CSS query language
         Element tableWth = page.select("table[class=wt]").first();
         Elements names = tableWth.select("tr[class=wth]"); //Говорим элементу, что из нашей таблицы мы хотим
         Elements values = tableWth.select("tr[valign=top]");
+        int index = 0; // Будет говорить на каком значении мы сейчас находимся.
         //выбрать только те tr у когорых класс wth
-
-        for (Element name: names) {
-            String date = name.select("th[id=dt").text();
+        for (Element name : names) {
+            String dateString = name.select("th[id=dt").text();
+            String date = getDateFromString(dateString);
             System.out.println(date + "    Явления    Температура    Давление    Влажность    Ветер");
+            int iterationCount = printPartValues(values, index);
+            index = index + iterationCount;
         }
     }
 }
+
